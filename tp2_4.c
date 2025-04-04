622f6ebb08ed615a71a7d308ebeaa3da4e5d102d
Sebastian Zelarayan <sebazeta7@gmail.com>
2025-04-04 13:12:03 -0300

    Funciones listar pc y auxiliar en tp2_4.c

--- tp2_4.c
+++ tp2_4.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define TAMA 10
-
+//defino estructura
 struct {
     int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3)
     int year; // Año de fabricación (valor entre 2015 y 2024)
@@ -15,13 +14,17 @@ struct {
 void listarPCs(Tcompu pc[], int cantidad);
 void mostrarMasVieja(Tcompu pc[], int cantidad);
 void mostrarMasVeloz(Tcompu pc[], int cantidad);
+//funcion auxiliar 
+void mostrarPC(Tcompu pc);
 
 int main()
 {
     //carga de datos
     Tcompu pc[5];
+    int cantidad;
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
     srand(time(NULL));
+    //cargo el arreglo pc con datos aleatorios
     for (int i = 0; i < 5; i++)
     {
         pc[i].velocidad=1+rand()%(3-1+1);
@@ -30,9 +33,33 @@ int main()
         pc[i].tipo_cpu=tipos+(rand()%6);
     }
 
+    //utilizo funciones
 
+    //funcion listar pcs
+    printf("Ingrese la cantidad de PC's a mostrar(<=5): \n");
+    scanf("%d",&cantidad);
+    listarPCs(pc,cantidad);
 
-    
     return 0;
 }
 //desarrollo de funciones
+//funcion auxiliar
+void mostrarPC(Tcompu pc){
+    
+    printf("Velocidad: %d GHz\n",pc.velocidad);
+    printf("Year: %d\n",pc.year);
+    printf("Nucleos: %d\n",pc.cantidad_nucleos);
+    printf("Tipo CPU: %s\n",pc.tipo_cpu);
+    printf("\n");
+}
+
+
+
+void listarPCs(Tcompu pc[], int cantidad){
+    for (int i = 0; i < cantidad; i++)
+    {
+        mostrarPC(pc[i]);
+    }
+    
+
+}
